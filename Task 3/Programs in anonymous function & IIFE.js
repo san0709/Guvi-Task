19006b203250eac5a4ff02894a7ef80e606794d0
san0709 <sandhiya6797@gmail.com>
2023-10-18 19:51:19 +0530

    save

--- Task 3/Programs in anonymous function & IIFE.js
+++ Task 3/Programs in anonymous function & IIFE.js
@@ -87,3 +87,30 @@
                 }
                 })();
     //e.Return all the palindromes in an array
+    function isPalindrome(N)
+    {
+        let str = "" + N;
+        let len = str.length;
+        for (let i = 0; i < parseInt(len / 2, 10); i++)
+        {
+            if (str[i] != str[len - 1 - i ])
+                return false;
+        }
+        return true;
+    }
+    
+  Anonymous Function :  function (arr, n)
+    {
+        // Traversing each element of the array
+        // and check if it is palindrome or not
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }
+
+    
+    
