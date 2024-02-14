6799735fea1e76901c714befeb44ac842c7e9620
san0709 <sandhiya6797@gmail.com>
2024-02-14 17:43:13 +0530

    update

---
+++ Guvi Task/Task 11/index.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById('output').innerHTML = 10;
+    setTimeout(() => {
+        document.getElementById('output').innerHTML = 9;
+        setTimeout(() => {
+            document.getElementById('output').innerHTML = 8;
+            setTimeout(() => {
+                document.getElementById('output').innerHTML = 7;
+                setTimeout(() => {
+                    document.getElementById('output').innerHTML = 6;
+                    setTimeout(() => {
+                        document.getElementById('output').innerHTML = 5;
+                        setTimeout(() => {
+                            document.getElementById('output').innerHTML = 4;
+                            setTimeout(() => {
+                                document.getElementById('output').innerHTML = 3;
+                                setTimeout(() => {
+                                    document.getElementById('output').innerHTML = 2;
+                                    setTimeout(() => {
+                                        document.getElementById('output').innerHTML = 1;
+                                        setTimeout(() => {
+                                            document.getElementById('output').innerHTML = "Happy Independence Day!";
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
